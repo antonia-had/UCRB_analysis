512a2b8c59a256e674e29b7027cee1bf1382c04c
antonia-had <ah986@cornell.edu>
2019-05-15 13:02:34 -0400

    draw delta only

--- Analysis_scripts/Output_analysis/shortage_duration_curves_global_experiment.py
+++ Analysis_scripts/Output_analysis/shortage_duration_curves_global_experiment.py
@@ -79,76 +79,76 @@ def plotSDC(synthetic, histData, structure_name):
     
     p=np.arange(100,-10,-10)
     p_i=p[::-1]
-    hist_durations_percentiles = np.zeros([len(p_i)])
-    multi_year_durations_percentiles = np.zeros([len(p_i),samples])
-    for i in range(samples):
-        for j in range(len(p_i)):
-            if hist_durations:
-                hist_durations_percentiles[j] = np.percentile(hist_durations,p_i[j])
-            if multi_year_durations[i]:
-                multi_year_durations_percentiles[j,i] = np.percentile(multi_year_durations[i],p_i[j])
-    
-    fig, (ax1) = plt.subplots(1,1, figsize=(14.5,8))
-    # ax1
-    handles = []
-    labels=[]
-    color = '#000292'
-    for i in range(len(p)):
-        ax1.fill_between(p_i, np.min(multi_year_durations_percentiles[:,:],1), np.percentile(multi_year_durations_percentiles[:,:], p[i], axis=1), color=color, alpha = 0.1)
-        ax1.plot(p_i, np.percentile(multi_year_durations_percentiles[:,:], p[i], axis=1), linewidth=0.5, color=color, alpha = 0.3)
-        handle = matplotlib.patches.Rectangle((0,0),1,1, color=color, alpha=alpha(i, base=0.1))
-        handles.append(handle)
-        label = "{:.0f} %".format(100-p[i])
-        labels.append(label)
-    ax1.plot(p_i,hist_durations_percentiles, c='black', linewidth=2, label='Historical record')
-    ax1.set_xlim(0,100)
-    ax1.legend(handles=handles, labels=labels, framealpha=1, fontsize=8, loc='upper left', title='Frequency in experiment',ncol=2)
-    ax1.set_xlabel('Shortage magnitude percentile', fontsize=12)
-    ax1.set_ylabel('Years of continuous shortages', fontsize=12)
-    fig.suptitle('Duration of shortage for ' + structure_name, fontsize=16)
-    fig.savefig('./MultiyearShortageCurves/' + structure_name + '.svg')
-    fig.savefig('./MultiyearShortageCurves/' + structure_name + '.png')
-    fig.clf()
+#    hist_durations_percentiles = np.zeros([len(p_i)])
+#    multi_year_durations_percentiles = np.zeros([len(p_i),samples])
+#    for i in range(samples):
+#        for j in range(len(p_i)):
+#            if hist_durations:
+#                hist_durations_percentiles[j] = np.percentile(hist_durations,p_i[j])
+#            if multi_year_durations[i]:
+#                multi_year_durations_percentiles[j,i] = np.percentile(multi_year_durations[i],p_i[j])
+#    
+#    fig, (ax1) = plt.subplots(1,1, figsize=(14.5,8))
+#    # ax1
+#    handles = []
+#    labels=[]
+#    color = '#000292'
+#    for i in range(len(p)):
+#        ax1.fill_between(p_i, np.min(multi_year_durations_percentiles[:,:],1), np.percentile(multi_year_durations_percentiles[:,:], p[i], axis=1), color=color, alpha = 0.1)
+#        ax1.plot(p_i, np.percentile(multi_year_durations_percentiles[:,:], p[i], axis=1), linewidth=0.5, color=color, alpha = 0.3)
+#        handle = matplotlib.patches.Rectangle((0,0),1,1, color=color, alpha=alpha(i, base=0.1))
+#        handles.append(handle)
+#        label = "{:.0f} %".format(100-p[i])
+#        labels.append(label)
+#    ax1.plot(p_i,hist_durations_percentiles, c='black', linewidth=2, label='Historical record')
+#    ax1.set_xlim(0,100)
+#    ax1.legend(handles=handles, labels=labels, framealpha=1, fontsize=8, loc='upper left', title='Frequency in experiment',ncol=2)
+#    ax1.set_xlabel('Shortage magnitude percentile', fontsize=12)
+#    ax1.set_ylabel('Years of continuous shortages', fontsize=12)
+#    fig.suptitle('Duration of shortage for ' + structure_name, fontsize=16)
+#    fig.savefig('./MultiyearShortageCurves/' + structure_name + '.svg')
+#    fig.savefig('./MultiyearShortageCurves/' + structure_name + '.png')
+#    fig.clf()
     
     #Calculate synthetic shortage duration curves
     F_syn = np.empty([int(np.size(histData)/n),samples])
     F_syn[:] = np.NaN
     for j in range(samples):
         F_syn[:,j] = np.sort(synthetic_global_totals[:,j])
-    
-    # For each percentile of magnitude, calculate the percentile among the experiments ran
-    perc_scores = np.zeros_like(F_syn) 
-    for m in range(int(np.size(histData)/n)):
-        perc_scores[m,:] = [stats.percentileofscore(F_syn[m,:], j, 'rank') for j in F_syn[m,:]]
-                
-    P = np.arange(1.,len(F_hist)+1)*100 / len(F_hist)
-    
-    
+#    
+#    # For each percentile of magnitude, calculate the percentile among the experiments ran
+#    perc_scores = np.zeros_like(F_syn) 
+#    for m in range(int(np.size(histData)/n)):
+#        perc_scores[m,:] = [stats.percentileofscore(F_syn[m,:], j, 'rank') for j in F_syn[m,:]]
+#                
+#    P = np.arange(1.,len(F_hist)+1)*100 / len(F_hist)
+#    
+#    
     ylimit = round(np.max(F_syn), -3)
-    fig, (ax1) = plt.subplots(1,1, figsize=(14.5,8))
-    # ax1
-    handles = []
-    labels=[]
-    color = '#000292'
-    for i in range(len(p)):
-        ax1.fill_between(P, np.min(F_syn[:,:],1), np.percentile(F_syn[:,:], p[i], axis=1), color=color, alpha = 0.1)
-        ax1.plot(P, np.percentile(F_syn[:,:], p[i], axis=1), linewidth=0.5, color=color, alpha = 0.3)
-        handle = matplotlib.patches.Rectangle((0,0),1,1, color=color, alpha=alpha(i, base=0.1))
-        handles.append(handle)
-        label = "{:.0f} %".format(100-p[i])
-        labels.append(label)
-    ax1.plot(P,F_hist, c='black', linewidth=2, label='Historical record')
-    ax1.set_ylim(0,ylimit)
-    ax1.set_xlim(0,100)
-    ax1.legend(handles=handles, labels=labels, framealpha=1, fontsize=8, loc='upper left', title='Frequency in experiment',ncol=2)
-    ax1.set_xlabel('Shortage magnitude percentile', fontsize=12)
-    ax1.set_ylabel('Annual shortage (af)', fontsize=12)
-
-    fig.suptitle('Shortage magnitudes for ' + structure_name, fontsize=16)
-    plt.subplots_adjust(bottom=0.2)
-    fig.savefig('./ShortagePercentileCurves/' + structure_name + '.svg')
-    fig.savefig('./ShortagePercentileCurves/' + structure_name + '.png')
-    fig.clf()
+#    fig, (ax1) = plt.subplots(1,1, figsize=(14.5,8))
+#    # ax1
+#    handles = []
+#    labels=[]
+#    color = '#000292'
+#    for i in range(len(p)):
+#        ax1.fill_between(P, np.min(F_syn[:,:],1), np.percentile(F_syn[:,:], p[i], axis=1), color=color, alpha = 0.1)
+#        ax1.plot(P, np.percentile(F_syn[:,:], p[i], axis=1), linewidth=0.5, color=color, alpha = 0.3)
+#        handle = matplotlib.patches.Rectangle((0,0),1,1, color=color, alpha=alpha(i, base=0.1))
+#        handles.append(handle)
+#        label = "{:.0f} %".format(100-p[i])
+#        labels.append(label)
+#    ax1.plot(P,F_hist, c='black', linewidth=2, label='Historical record')
+#    ax1.set_ylim(0,ylimit)
+#    ax1.set_xlim(0,100)
+#    ax1.legend(handles=handles, labels=labels, framealpha=1, fontsize=8, loc='upper left', title='Frequency in experiment',ncol=2)
+#    ax1.set_xlabel('Shortage magnitude percentile', fontsize=12)
+#    ax1.set_ylabel('Annual shortage (af)', fontsize=12)
+#
+#    fig.suptitle('Shortage magnitudes for ' + structure_name, fontsize=16)
+#    plt.subplots_adjust(bottom=0.2)
+#    fig.savefig('./ShortagePercentileCurves/' + structure_name + '.svg')
+#    fig.savefig('./ShortagePercentileCurves/' + structure_name + '.png')
+#    fig.clf()
     
     '''
     Sensitivity analysis plots
@@ -172,71 +172,72 @@ def plotSDC(synthetic, histData, structure_name):
         delta_values[column] = delta_values[column].round(decimals = 2)
     delta_values_to_plot = delta_values.values.tolist()
     
-    S1_values = pd.read_csv('./Magnitude_Sensitivity_analysis/'+ structure_name + '_S1.csv')
-    S1_values.set_index(list(S1_values)[0],inplace=True)
-    S1_values = S1_values.clip(lower=0)
-    bottom_row = pd.DataFrame(data=np.array([np.zeros(100)]), index= ['Interaction'], columns=list(S1_values.columns.values))
-    top_row = pd.DataFrame(data=np.array([globalmin]), index= ['Min'], columns=list(S1_values.columns.values))
-    S1_values = pd.concat([top_row,S1_values.loc[:],bottom_row])
-    for p in percentiles:
-        total = np.sum(S1_values[str(p)])-S1_values.at['Min',str(p)]
-        if total!=0:
-            diff = 1-total
-            S1_values.set_value('Interaction',str(p),diff)
-            for param in param_names+['Interaction']:
-                value = (globalmax[p]-globalmin[p])*S1_values.at[param,str(p)]
-                S1_values.set_value(param,str(p),value)                
-    for column in S1_values:
-        S1_values[column] = S1_values[column].round(decimals = 2)
-    S1_values_to_plot = S1_values.values.tolist()
-
-    R2_values = pd.read_csv('./Magnitude_Sensitivity_analysis/'+ structure_name + '_R2.csv')
-    R2_values.set_index(list(R2_values)[0],inplace=True)
-    R2_values = R2_values.clip(lower=0)
-    bottom_row = pd.DataFrame(data=np.array([np.zeros(100)]), index= ['Interaction'], columns=list(R2_values.columns.values))
-    top_row = pd.DataFrame(data=np.array([globalmin]), index= ['Min'], columns=list(R2_values.columns.values))
-    R2_values = pd.concat([top_row,R2_values.loc[:],bottom_row])
-    for p in percentiles:
-        total = np.sum(R2_values[str(p)])-R2_values.at['Min',str(p)]
-        if total!=0:
-            diff = 1-total
-            R2_values.set_value('Interaction',str(p),diff)
-            for param in param_names+['Interaction']:
-                value = (globalmax[p]-globalmin[p])*R2_values.at[param,str(percentiles[p])]
-                R2_values.set_value(param,str(percentiles[p]),value)
-    for column in R2_values:
-        R2_values[column] = R2_values[column].round(decimals = 2)
-    R2_values_to_plot = R2_values.values.tolist()
+#    S1_values = pd.read_csv('./Magnitude_Sensitivity_analysis/'+ structure_name + '_S1.csv')
+#    S1_values.set_index(list(S1_values)[0],inplace=True)
+#    S1_values = S1_values.clip(lower=0)
+#    bottom_row = pd.DataFrame(data=np.array([np.zeros(100)]), index= ['Interaction'], columns=list(S1_values.columns.values))
+#    top_row = pd.DataFrame(data=np.array([globalmin]), index= ['Min'], columns=list(S1_values.columns.values))
+#    S1_values = pd.concat([top_row,S1_values.loc[:],bottom_row])
+#    for p in percentiles:
+#        total = np.sum(S1_values[str(p)])-S1_values.at['Min',str(p)]
+#        if total!=0:
+#            diff = 1-total
+#            S1_values.set_value('Interaction',str(p),diff)
+#            for param in param_names+['Interaction']:
+#                value = (globalmax[p]-globalmin[p])*S1_values.at[param,str(p)]
+#                S1_values.set_value(param,str(p),value)                
+#    for column in S1_values:
+#        S1_values[column] = S1_values[column].round(decimals = 2)
+#    S1_values_to_plot = S1_values.values.tolist()
+#
+#    R2_values = pd.read_csv('./Magnitude_Sensitivity_analysis/'+ structure_name + '_R2.csv')
+#    R2_values.set_index(list(R2_values)[0],inplace=True)
+#    R2_values = R2_values.clip(lower=0)
+#    bottom_row = pd.DataFrame(data=np.array([np.zeros(100)]), index= ['Interaction'], columns=list(R2_values.columns.values))
+#    top_row = pd.DataFrame(data=np.array([globalmin]), index= ['Min'], columns=list(R2_values.columns.values))
+#    R2_values = pd.concat([top_row,R2_values.loc[:],bottom_row])
+#    for p in percentiles:
+#        total = np.sum(R2_values[str(p)])-R2_values.at['Min',str(p)]
+#        if total!=0:
+#            diff = 1-total
+#            R2_values.set_value('Interaction',str(p),diff)
+#            for param in param_names+['Interaction']:
+#                value = (globalmax[p]-globalmin[p])*R2_values.at[param,str(percentiles[p])]
+#                R2_values.set_value(param,str(percentiles[p]),value)
+#    for column in R2_values:
+#        R2_values[column] = R2_values[column].round(decimals = 2)
+#    R2_values_to_plot = R2_values.values.tolist()
     
     color_list = ["white", "#F18670", "#E24D3F", "#CF233E", "#681E33", "#676572", "#F3BE22", "#59DEBA", "#14015C", "#DAF8A3", "#0B7A0A", "#F8FFA2", "#578DC0", "#4E4AD8", "#32B3F7","#F77632"]     
     
-    fig, (ax1, ax2, ax3) = plt.subplots(1,3, figsize=(14.5,8))
+    #fig, (ax1, ax2, ax3) = plt.subplots(1,3, figsize=(14.5,8))
+    fig, (ax1) = plt.subplots(1,1, figsize=(14.5,8))
     ax1.stackplot(np.arange(0,100), delta_values_to_plot, colors = color_list)
     ax1.plot(percentiles, globalmax, color='black', linewidth=1)
     ax1.plot(percentiles, globalmin, color='black', linewidth=1)
     ax1.set_title("Delta index")
     ax1.set_ylim(0,ylimit)
     ax1.set_xlim(0,100)
-    ax2.stackplot(np.arange(0,100), S1_values_to_plot, colors = color_list)
-    ax2.plot(percentiles, globalmax, color='black', linewidth=1)
-    ax2.plot(percentiles, globalmin, color='black', linewidth=1)
-    ax2.set_title("S1")
-    ax2.set_ylim(0,ylimit)
-    ax2.set_xlim(0,100)
-    ax3.stackplot(np.arange(0,100), R2_values_to_plot, colors = color_list)
-    ax3.plot(percentiles, globalmax, color='black', linewidth=1)
-    ax3.plot(percentiles, globalmin, color='black', linewidth=1)
-    ax3.set_title("R^2")
-    ax3.set_ylim(0,ylimit)
-    ax3.set_xlim(0,100)
-    handles, labels = ax3.get_legend_handles_labels()
+#    ax2.stackplot(np.arange(0,100), S1_values_to_plot, colors = color_list)
+#    ax2.plot(percentiles, globalmax, color='black', linewidth=1)
+#    ax2.plot(percentiles, globalmin, color='black', linewidth=1)
+#    ax2.set_title("S1")
+#    ax2.set_ylim(0,ylimit)
+#    ax2.set_xlim(0,100)
+#    ax3.stackplot(np.arange(0,100), R2_values_to_plot, colors = color_list)
+#    ax3.plot(percentiles, globalmax, color='black', linewidth=1)
+#    ax3.plot(percentiles, globalmin, color='black', linewidth=1)
+#    ax3.set_title("R^2")
+#    ax3.set_ylim(0,ylimit)
+#    ax3.set_xlim(0,100)
+    handles, labels = ax1.get_legend_handles_labels()
     ax1.set_ylabel('Annual shortage (af)', fontsize=12)
-    ax2.set_xlabel('Shortage magnitude percentile', fontsize=12)
+    ax1.set_xlabel('Shortage magnitude percentile', fontsize=12)
     fig.legend(handles[1:], labels = parameter_names_long[1:], fontsize=10, loc='lower center',ncol = 5)
     plt.subplots_adjust(bottom=0.2)
     fig.suptitle('Shortage magnitude sensitivity for '+ structure_name, fontsize=16)
-    fig.savefig('./ShortageSensitivityCurves/' + structure_name + '.svg')
-    fig.savefig('./ShortageSensitivityCurves/' + structure_name + '.png')
+    fig.savefig('./ShortageSensitivityCurves/' + structure_name + '_delta.svg')
+    fig.savefig('./ShortageSensitivityCurves/' + structure_name + '_delta.png')
 
 def getinfo(ID):
     line_out = '' #Empty line for storing data to print in file   
