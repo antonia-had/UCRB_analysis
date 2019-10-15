60ee23243d12c5dd949c1937b3f1c0223db36e36
antonia-had <ah986@cornell.edu>
2019-10-15 17:18:32 -0400

    rearranging

--- Output_analysis/shortage_curves.sh
+++ Output_analysis/shortage_curves.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --partition=debug
 #SBATCH --nodes=1             # specify number of nodes
-#SBATCH --ntasks-per-node=1  # specify number of core per node
+#SBATCH --ntasks-per-node=24  # specify number of core per node
 #SBATCH --export=ALL
 #SBATCH -t 0:30:00            # set max wallclock time
-#SBATCH --job-name="statemod" # name your job
-#SBATCH --output="output.out"
+#SBATCH --job-name="curves" # name your job
+#SBATCH --output="output/curves.out"
 
 module load python
 module load mpi4py
--- README.txt
+++ README.txt
@@ -4,3 +4,4 @@ Statemod_files: Contains all StateMod files needed to generate inputs for experi
 SALib: Python library for sensitivity analysis used by some experiments (not synced on GitHub)
 Structures_files: Contains information files about structures in the basin
 Summary_info: Contains summary data from historical record and experiments
+Old_data: Contains old compressed analysis data (not synced on GitHub)
