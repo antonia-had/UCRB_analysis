3fb354778cfce30084994032fd5794ca22f5fc36
antonia-had <ah986@cornell.edu>
2019-10-16 09:48:18 -0400

    generalized streamflow boxplots to accept any design as input

--- Input_simulation/experiment_streamflow_boxplots.py
+++ Input_simulation/experiment_streamflow_boxplots.py
@@ -1,11 +1,14 @@
 from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
-#plt.switch_backend('agg')
-#plt.ioff()
+import sys
+plt.switch_backend('agg')
+plt.ioff()
+
+design = str(sys.argv[1])
 
 # load paleo data at Cisco
-Paleo = pd.read_csv('./Cisco_Recon_v_Observed_v_Stateline.csv')
+Paleo = pd.read_csv('../Summary_info/Cisco_Recon_v_Observed_v_Stateline.csv')
 
 # re-scale Cisco data to estimate data at CO-UT state line
 factor = np.nanmean(Paleo['ObservedNaturalStateline']/Paleo['ObservedNaturalCisco'])
@@ -16,7 +19,7 @@ years = np.arange(1909, 2014)
 years_s = np.arange(1950, 2014)
 
 # Load historic data
-historic_file = open('./Experiment_files/cm2015x.xbm', 'r')
+historic_file = open('./cm2015x.xbm', 'r')
 all_split_data = [x.split('.') for x in historic_file.readlines()]
 historic_data = np.zeros([len(years),12])
 count = 0
@@ -28,6 +31,8 @@ for i in range(16, len(all_split_data)):
         historic_data[count,:] = [int(j) for j in data_to_write]
         count+=1
         
+np.save('../Summary_info/historic_flows.npy', historic_data)
+        
 # Load CMIP flows
 CMIP3_flows = np.genfromtxt('CMIP3_flows.csv', delimiter=',')
 CMIP3_flows = np.reshape(CMIP3_flows, [112, 64,12])
@@ -35,27 +40,27 @@ CMIP5_flows = np.genfromtxt('CMIP5_flows.csv', delimiter=',')
 CMIP5_flows = np.reshape(CMIP5_flows, [97, 64,12])
 
 # Load synthetic stationary flows
-baseCase = np.load('Sample1_Flows_logspace.npy')[:,-1,:]
+baseCase = np.load('../Summary_info/Sample1_Flows_logspace.npy')[:,-1,:]
 
 # Load synthetic nonstationary runs
-#synthetic_flows = np.zeros([10000, len(years),12])
-#for s in range(1000):
-#    for k in range(10):       
-#        synthetic_file = open('./Experiment_files/cm2015x_S'+str(s+1)+'_'+str(k+1)+'.xbm', 'r')
-#        all_split_data = [x.split('.') for x in synthetic_file.readlines()]
-#        yearcount = 0
-#        for i in range(16, len(all_split_data)):
-#            row_data = []
-#            row_data.extend(all_split_data[i][0].split())
-#            if row_data[1] == '09163500':
-#                data_to_write = [row_data[2]]+all_split_data[i][1:12]
-#                synthetic_flows[s*10+k,yearcount,:] = [int(j) for j in data_to_write]
-#                yearcount+=1
-#            
+synthetic_flows = np.zeros([10000, len(years),12])
+for s in range(1000):
+    for k in range(10):       
+        synthetic_file = open('../' + design + '/Experiment_files/cm2015x_S'+str(s+1)+'_'+str(k+1)+'.xbm', 'r')
+        all_split_data = [x.split('.') for x in synthetic_file.readlines()]
+        yearcount = 0
+        for i in range(16, len(all_split_data)):
+            row_data = []
+            row_data.extend(all_split_data[i][0].split())
+            if row_data[1] == '09163500':
+                data_to_write = [row_data[2]]+all_split_data[i][1:12]
+                synthetic_flows[s*10+k,yearcount,:] = [int(j) for j in data_to_write]
+                yearcount+=1
+            
 #synthetic_flows = np.delete(synthetic_flows,75,1)
-#np.save('syntheticflows.npy', synthetic_flows)
+np.save('../Summary_info/'+design+'_flows.npy', synthetic_flows)
 
-synthetic_flows = np.load('syntheticflows.npy')
+#synthetic_flows = np.load('syntheticflows.npy')
 
 
 colors = ['#AA1209','#DD7373', '#305252', '#3C787E','#D0CD94', '#9597a3']
@@ -71,7 +76,7 @@ for i in range(len(boxplots['boxes'])):
 ax.set_ylabel('Flow at Last Node (af)',fontsize=20)
 ax.set_yticklabels(['{:,}'.format(int(x)) for x in ax.get_yticks().tolist()],fontsize=16)
 ax.set_xticklabels(labels,fontsize=16)
-plt.savefig('streamflow_boxplots.svg')
+plt.savefig('../Summary_info/streamflow_boxplot_'+design+'.svg')
 
 fig = plt.figure(figsize=(18,9))
 ax = fig.add_subplot(111)
@@ -89,5 +94,5 @@ ax.set_ylabel('Flow at Last Node (af)',fontsize=20)
 ax.set_xticks(np.arange(1,7))
 ax.set_xticklabels(labels,fontsize=16)
 ax.set_yticklabels(['{:,}'.format(int(x)) for x in ax.get_yticks().tolist()],fontsize=16)
-plt.savefig('violin_boxplots.svg')
+plt.savefig('../Summary_info/streamflow_violinplot_'+design+'.svg')
 
