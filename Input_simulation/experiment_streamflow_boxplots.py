ebfcd6c2c5fc2aedd3c7a3cc4a216c53efc19e99
antonia-had <ah986@cornell.edu>
2019-10-16 09:53:30 -0400

    fix directories

--- Input_simulation/experiment_streamflow_boxplots.py
+++ Input_simulation/experiment_streamflow_boxplots.py
@@ -19,24 +19,25 @@ years = np.arange(1909, 2014)
 years_s = np.arange(1950, 2014)
 
 # Load historic data
-historic_file = open('./cm2015x.xbm', 'r')
-all_split_data = [x.split('.') for x in historic_file.readlines()]
-historic_data = np.zeros([len(years),12])
-count = 0
-for i in range(16, len(all_split_data)):
-    row_data = []
-    row_data.extend(all_split_data[i][0].split())
-    if row_data[1] == '09163500':
-        data_to_write = [row_data[2]]+all_split_data[i][1:12]
-        historic_data[count,:] = [int(j) for j in data_to_write]
-        count+=1
-        
-np.save('../Summary_info/historic_flows.npy', historic_data)
+#historic_file = open('../Summary_info/cm2015x.xbm', 'r')
+#all_split_data = [x.split('.') for x in historic_file.readlines()]
+#historic_data = np.zeros([len(years),12])
+#count = 0
+#for i in range(16, len(all_split_data)):
+#    row_data = []
+#    row_data.extend(all_split_data[i][0].split())
+#    if row_data[1] == '09163500':
+#        data_to_write = [row_data[2]]+all_split_data[i][1:12]
+#        historic_data[count,:] = [int(j) for j in data_to_write]
+#        count+=1
+#        
+#np.save('../Summary_info/historic_flows.npy', historic_data)
+historic_data = np.load('../Summary_info/historic_flows.npy')
         
 # Load CMIP flows
-CMIP3_flows = np.genfromtxt('CMIP3_flows.csv', delimiter=',')
+CMIP3_flows = np.genfromtxt('../Summary_info/CMIP3_flows.csv', delimiter=',')
 CMIP3_flows = np.reshape(CMIP3_flows, [112, 64,12])
-CMIP5_flows = np.genfromtxt('CMIP5_flows.csv', delimiter=',')
+CMIP5_flows = np.genfromtxt('../Summary_info/CMIP5_flows.csv', delimiter=',')
 CMIP5_flows = np.reshape(CMIP5_flows, [97, 64,12])
 
 # Load synthetic stationary flows
