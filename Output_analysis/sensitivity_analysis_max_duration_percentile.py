92b62946a0065e7298df70baf07b6745b0f7885d
antonia-had <ah986@cornell.edu>
2020-03-24 16:35:26 -0400

    back to the 99th percentile

--- Output_analysis/sensitivity_analysis_max_duration_percentile.py
+++ Output_analysis/sensitivity_analysis_max_duration_percentile.py
@@ -74,7 +74,7 @@ problem = {
     'names': param_names,
     'bounds': param_bounds.tolist()
 }
-percentiles = np.arange(0,101)
+percentiles = np.arange(0,100)
 all_IDs = np.genfromtxt('../Structures_files/metrics_structures.txt',dtype='str').tolist() 
 nStructures = len(all_IDs)
 
